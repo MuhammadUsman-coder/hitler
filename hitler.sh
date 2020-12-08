d7cf9e60beaca4b33684624bebf2e061db4f84dc
root <root@kali>
2020-12-08 09:04:45 -0500

    Added something

--- hitler.sh
+++ hitler.sh
@@ -1,1 +1,181 @@
-test
+#!/bin/bash
+
+target=$1;
+
+echo "[+] Checking tools required for subdomain enumeration."
+echo "[+] Making directory to install tools, all tools that required repositories to be cloned will be made in ./bugbounty/tools"
+
+# for i in {"./bugbounty/$target/recon/subdomain/"};do
+#         mkdir -p $i;
+# done
+
+mkdir -p ./bugbounty/$target/recon/subdomain/
+echo "[+] Starting subdomain enumeration."
+
+echo "[+] Starting sublist3r for subdomain enumeration."
+sublist3r -d "$target"  > ./bugbounty/$target/recon/subdomain/sublist3r.txt
+
+echo "[+] Starting amass for subdomain enumeration."
+amass enum -passive -d "$target" > ./bugbounty/$target/recon/subdomain/amass.txt
+
+echo "[+] Starting find-domain for subdomain enumeration."
+python3 ~/tools/findSubDomains/findSubDomains.py "$target" -t 200 > ./bugbounty/$target/recon/subdomain/findSubs.txt
+
+echo "[+] Running subscraper for subdomain enumeration."
+subscraper -t 100 "$target" --censys-api "29c6e13b-bdd2-46d8-b6b7-1f679b4ee70d" --censys-secret "CfEDWOxM2OLR8CYzfNnK0V46E2oLcokd"| grep "$target" |cut -f1 | grep "$target"| awk '{print $2}'  > ./bugbounty/$target/recon/subdomain/subscraper.txt
+
+echo "[+] Running SubDomainizer for subdomain enumeration."
+python3 ~/tools/SubDomainizer/SubDomainizer.py -u http://$target -o  ./bugbounty/$target/recon/subdomain/subdominizer.txt > /dev/null 2>&1
+
+echo "[+] Running subdomains-scanner for subdomain enumeration."
+subdomain-scanner -depth 3 -d $target -t 300 -f ~/go/src/github.com/fengdingbo/subdomain-scanner/dict/subnames_full.txt -o ./bugbounty/$target/recon/subdomain/subdomain-scanner.txt 
+
+echo "[+] Running chaos for subdomain enumeration."
+export CHAOS_KEY="a01b4a1eae3ec7ea5293318487e2aa83208c428103740e553ddde3bac8e412d1"
+chaos -d $target -silent|grep $target > ./bugbounty/$target/recon/subdomain/chaos.txt
+
+echo "[+] Running assetfinder for subdomain enumeration."
+assetfinder --subs-only $target > ./bugbounty/$target/recon/subdomain/assetfinder.txt
+
+echo "[+] Running delator for subdomain enumeration."
+delator -d $target -s crt > ./bugbounty/$target/recon/subdomain/delator.txt
+
+echo "[+] Running vita for subdomain enumeration."
+~/tools/vita/vita-0.1.14-x86_64-unknown-linux-musl/vita -d $target -c 300 > ./bugbounty/$target/recon/subdomain/vita.txt
+
+echo "[+] Running subfinder for subdomain enumeration."
+subfinder -silent -d $target -t 50 > ./bugbounty/$target/recon/subdomain/subfinder.txt
+
+echo "[+] Running nmap dns-bruteforcing for subdomain enumeration."
+nmap --script dns-brute $target | grep $target |tail -n +3 | cut -c 7- | cut -f1 -d ' '| uniq > ./bugbounty/$target/recon/subdomain/nmap.txt
+
+echo "[+] Running dnscan for subdomain enumeration."
+python3 ~/tools/dnscan/dnscan.py -t 32 -d $target -w ./bugbounty/tools/dnscan/subdomains-10000.txt  -n -t 50  | grep .$target | tail -n +3 | cut -f3 -d ' ' > /dev/null 2>&1
+
+#echo "[+] Running rush with seclist for subdomain enumeration."
+#rush -j200 -i ./bugbounty/tools/best-dns-wordlist.txt  ' curl -s -L "https://dns.google.com/resolve?name={}."'"$target.com"'"&type=A&cd=true" | sed "s#\"#\n# g;s# #\n#g" | grep "'"$target"'"' | sed ' s#\.$##g' |httpx| anew ./bugbounty/$target/recon/subdomain/rush.txt
+cat ./bugbounty/$target/recon/subdomain/* |sort -u > ./bugbounty/$target/recon/subdomain/subs.txt
+
+echo "[+] Starting github-subdomain.py for subdomain enumeration."
+pytho3 github-search/github-subdomains.py -t e5ee760a9e3e84b42282bf7d744dca095b7f9c34  -d "$target" > ./bugbounty/$target/recon/subdomain/github-subdomains.txt
+
+echo "[+] Starting crobat for subdomain enumeration."
+crobat -s "$target"
+
+echo "[+] Getting DNS SOA records."
+dnsx -t 400 -silent -l ./bugbounty/$target/recon/subdomain/subs.txt -soa -o ./bugbounty/$target/recon/subdomain/soa.txt
+
+echo "[+] Getting DNS MX records."
+dnsx -t 400 -silent -l ./bugbounty/$target/recon/subdomain/subs.txt -mx -o ./bugbounty/$target/recon/subdomain/mx.txt
+
+echo "[+] Getting DNS TXT records."
+dnsx -t 400 -silent -l ./bugbounty/$target/recon/subdomain/subs.txt -txt -o ./bugbounty/$target/recon/subdomain/txt.txt
+
+echo "[+] Getting DNS PTR records."
+dnsx -t 400 -silent -l ./bugbounty/$target/recon/subdomain/subs.txt -ptr -o ./bugbounty/$target/recon/subdomain/ptr.txt
+
+echo "[+] Getting DNS NS records."
+dnsx -t 400 -silent -l ./bugbounty/$target/recon/subdomain/subs.txt -ns -o ./bugbounty/$target/recon/subdomain/ns.txt
+
+echo "[+] Getting DNS A records."
+dnsx -t 400 -silent -l ./bugbounty/$target/recon/subdomain/subs.txt -a -o ./bugbounty/$target/recon/subdomain/a.txt
+
+echo "[+] Getting DNS AAAA records."
+dnsx -t 400 -silent -l ./bugbounty/$target/recon/subdomain/subs.txt -aaaa -o ./bugbounty/$target/recon/subdomain/aaaa.txt
+
+echo "[+] Getting DNS CNAME records."
+dnsx -t 400 -silent -l ./bugbounty/$target/recon/subdomain/subs.txt -cname -o ./bugbounty/$target/recon/subdomain/cname.txt
+
+cat ./bugbounty/$target/recon/subdomain/* | sort -u > ./bugbounty/$target/recon/subdomain/final2_subs.txt
+
+echo "[+] Starting shuffle dns to enumerate valid subdomains."
+shuffledns -silent -list ./bugbounty/$target/recon/subdomain/final2_subs.txt  -r ~/tools/resolvers.txt -o ./bugbounty/$target/recon/subdomain/shuffledns.txt
+
+#echo "[+] Starting ffuf for vhost bruteforce."
+#ffuf -w wordlist -u http://"$target"/ -H "Host:http://FUZZ.$target" -k -mc 200,204,301,302,307,401,403,500,404 -t 50 -o ./bugbounty/$target/recon/subdomain/vhost.txt
+
+cat ./bugbounty/$target/recon/subdomain/* | sort -u > ./bugbounty/$target/recon/subdomain/allsubdomains.txt
+
+echo "[+] Starting subjack for subdomain takeover."
+subjack -t 50 -w ./bugbounty/$target/recon/subdomain/allsubdomains.txt -a -ssl -t 50 -v -c ~/go/src/github.com/haccer/subjack/fingerprints.json -o ./bugbounty/$target/recon/subjack.txt grep -v "Not Vulnerable" 
+
+echo "[+] Starting nuclei for subdomain takeover."
+nuclei -c 50 -silent -l ./bugbounty/$target/recon/subdomain/allsubdomains.txt -t ~/tools/nuclei-templates/subdomain-takeover -c 50 -o ./bugbounty/$target/recon/nuclie/nuclei-takeover.txt
+
+echo "[+] Starting httpx for alive subdomains check."
+cat ./bugbounty/$target/recon/subdomain/allsubdomains.txt |httpx -threads 300 -silent -o ./bugbounty/$target/recon/subdomain/alive.txt
+
+echo "[+] Checking for subdomains redirection."
+a="./bugbounty/$target/recon/subdomain/alive.txt"
+cat $a | while read a; do
+    if curl -Is $a | grep "Location"; then
+        echo "$a redirects to `curl -Is $a | grep "Location"`" > ./bugbounty/$target/recon/subdomain/redirections.txt
+echo
+    else
+        echo "$a (-_-)"
+echo
+    fi
+done
+
+echo "[+] Starting nuclie to check for used technologies."
+nuclei -silent -l $targets -t ~/tools/nuclie-templates/technologies/ -c 50 -o ./bugbounty/$target/recon/nuclie/technologies.txt
+
+echo "[+] Starting nuclie with generic-detections template."
+nuclei -silent -l alive.txt -t ~/tools/nuclie-templates/generic-detections/ -c 50 -o ./bugbounty/$target/recon/nuclie/generic-detections.txt
+
+echo "[+] Starting nuclie with CVES template."
+nuclei -silent -l alive.txt -t ~/tools/nuclie-templates/cves/ -c 50 -o ./bugbounty/$target/recon/nuclie/cves.txt
+
+echo "[+] Starting nuclie with default-credentials template."
+nuclei -silent -l alive.txt -t ~/tools/nuclie-templates/default-credentials/ -c 50 -o ./bugbounty/$target/recon/nuclie/default-credentials.txt
+
+echo "[+] Starting nuclie with dns template."
+nuclei -silent -l alive.txt -t ~/tools/nuclie-templates/dns/ -c 50 -o ./bugbounty/$target/recon/nuclie/dns.txt
+
+echo "[+] Starting nuclie with files template."
+nuclei -silent -l alive.txt -t ~/tools/nuclie-templates/files/ -c 50 -o ./bugbounty/$target/recon/nuclie/files.txt
+
+echo "[+] Starting nuclie with panels template."
+nuclei -silent -l alive.txt -t ~/tools/nuclie-templates/panels/ -c 50 -o ./bugbounty/$target/recon/nuclie/panels.txt
+
+echo "[+] Starting nuclie with security-misconfiguration template."
+nuclei -silent -l alive.txt -t ~/tools/nuclie-templates/security-misconfiguration/ -c 50 -o ./bugbounty/$target/recon/nuclie/security-misconfiguration.txt
+
+echo "[+] Starting nuclie with tokens template."
+nuclei -silent -l alive.txt -t ~/tools/nuclie-templates/tokens/ -c 50 -o ./bugbounty/$target/recon/nuclie/tokens.txt
+
+echo "[+] Starting nuclie with vulnerabilities template."
+nuclei -silent -l alive.txt -t ~/tools/nuclie-templates/vulnerabilities/ -c 50 -o ./bugbounty/$target/recon/nuclie/vulnerabilities.txt
+
+echo "[+] Starting webanalyze for technologies enumeration."
+webanalyze -host shuftipro.com -crawl 1 -silent -worker 20 > ./bugbounty/$target/recon/webanalyze.txt
+
+echo "[+] Starting GAU for link enumeration."
+cat alive.txt | sed 's/https\?:\/\///' | gau > ./bugbounty/$target/recon/getallurls.txt
+
+echo "[+] Starting nuclie with generic-detections template."
+cat ./bugbounty/$target/recon/getallurls.txt | sort -u | unfurl --unique keys > ./bugbounty/$target/recon/paramlist.txt
+
+echo "[+] Starting nuclie with generic-detections template."
+cat ./bugbounty/$target/recon/getallurls.txt | sort -u | grep -P "\w+\.js(\?|$)" | httpx -silent -status-code -mc 200 | awk '{print $1}' | sort -u > jsurls.txt
+
+echo "[+] Starting nuclie with generic-detections template."
+cat ./bugbounty/$target/recon/getallurls.txt | sort -u | grep -P "\w+\.php(\?|$)" | httpx -silent -status-code -mc 200 | awk '{print $1}' | sort -u  > phpurls.txt
+
+echo "[+] Starting nuclie with generic-detections template."
+cat ./bugbounty/$target/recon/getallurls.txt | sort -u | grep -P "\w+\.aspx(\?|$) | httpx -silent -status-code -mc 200 | awk '{print $1}' | sort -u " > "$ARCHIVE"/aspxurls.txt
+
+echo "[+] Starting nuclie with generic-detections template."
+cat alive.txt | sort -u | grep -P "\w+\.jsp(\?|$) | httpx -silent -status-code -mc 200 | awk '{print $1}' | sort -u " > jspurls.txt
+
+echo "[+] Starting nuclie with generic-detections template."
+echo -e "[$GREEN+$RESET] fetchArchive finished"
+
+echo "[+] Starting nuclie with generic-detections template."
+cat alive.txt | ./CorsMe -t 70
+
+echo "[+] Starting nuclie with generic-detections template."
+crlfuzz -l file.txt >> saved.txt
+
+
+
---
+++ install.sh
@@ -0,0 +1,211 @@
+
+#!/bin/bash
+
+echo "[+] All tools will be installed in ~/tools."
+mkdir ~/tools
+cd ~/tools
+sudo apt-get -y update
+
+if ! [ -x "$(command -v go)" ];then 
+  echo "go not installed";
+  echo "Installing Golang"
+  wget https://golang.org/dl/go1.15.6.linux-amd64.tar.gz
+  sudo tar -xf go1.15.6.linux-amd64.tar.gz
+  sudo mv go /usr/local
+  export GOROOT=/usr/local/go
+  export GOPATH=$HOME/go
+  export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+  echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
+  echo 'export GOPATH=$HOME/go'    >> ~/.bash_profile            
+  echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile    
+  source ~/.bash_profile
+fi
+
+# if ! [ -d ~/go ];then
+#   echo "go directory not found in home directory."
+# fi
+
+if ! [ -x "$(command -v amass)" ];then 
+sudo apt install -y snapd 
+sudo snap install amass
+fi 
+
+if ! [ -x "$(command -v sublist3r)" ];then 
+echo "[+] Installing Sublist3r"
+git clone https://github.com/aboul3la/Sublist3r.git
+cd Sublist3r*
+pip install -r requirements.txt
+cd ~/tools/
+sudo pip3 install requests
+sudo pip3 install dnspython
+sudo pip3 install argparse
+echo "[+] Done"
+fi;
+
+echo "[+] downloading v-Host wordlist."
+curl https://raw.githubusercontent.com/codingo/VHostScan/master/VHostScan/wordlists/virtual-host-scanning.txt > v-host.txt
+
+# echo "[+] Installing GO."
+# sudo apt-get install golang -y
+# echo "export PATH=$PATH:~/go/bin" >> ~/.profile
+# source ~/.profile
+
+echo "[+] Installing findSubdomains."
+git clone https://github.com/starnightcyber/findSubDomains.git > /dev/null 2>&1
+python3 -m pip install -r findSubDomains/requirements.txt > /dev/null 2>&1
+echo "[+] Done."
+
+echo "[+] Installing subscraper."
+git clone https://github.com/m8r0wn/subscraper  > /dev/null 2>&1
+pip3 -q install -r subscraper/requirements.txt  > /dev/null 2>&1
+cd subscraper/
+python3 setup.py install > /dev/null 2>&1
+cd ..;
+echo "[+] Done."
+
+echo "[+] Installing SubDomainizer."
+git clone  https://github.com/nsonaniya2010/SubDomainizer.git  > /dev/null 2>&1
+pip3 -q install -r subdomanizer/requirements.txt > /dev/null 2>&1
+echo "[+] Done"
+
+echo "[+] Installing subdomains-scanner."
+go get github.com/miekg/dns > /dev/null 2>&1
+go get github.com/hashicorp/go-multierror > /dev/null 2>&1
+go get github.com/fengdingbo/subdomain-scanner > /dev/null 2>&1
+make -C ~/go/src/github.com/fengdingbo/subdomain-scanner/ 
+echo "[+] Done"    
+ 
+echo "[+] Installing assetfinder."
+go get -u github.com/tomnomnom/assetfinder
+echo "[+] Done."
+
+echo "[+] Installing chaos."
+GO111MODULE=on go get -u github.com/projectdiscovery/chaos-client/cmd/chaos
+echo "[+] Done."
+
+echo "[+] Installing delator."
+go get github.com/netevert/delator
+go build ~/go/src/github.com/netevert/delator/delator.go
+echo "[+] Done."
+
+echo "[+] Installing rush."
+go get -u github.com/shenwei356/rush/
+echo "[+] Done."
+ 
+echo "[+] Installing anew."
+go get -u github.com/tomnomnom/anew
+echo "[+] Done."
+
+echo "[+] Installing vita."
+wget -q https://github.com/junnlikestea/vita/releases/download/0.1.14/vita-0.1.14-x86_64-unknown-linux-musl.tar.gz
+tar -xf vita-0.1.14-x86_64-unknown-linux-musl.tar.gz
+echo "[+] Done."
+
+echo "[+] Installing subfinder."
+GO111MODULE=on go get -u -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+echo "[+] Done."
+
+echo "[+] Installing dnsx."
+GO111MODULE=on go get -u -v github.com/projectdiscovery/dnsx/cmd/dnsx
+echo "[+] Done."
+
+echo "[+] Installing shuffledns"
+GO111MODULE=on go get -u -v github.com/projectdiscovery/shuffledns/cmd/shuffledns
+echo "[+] Done."
+
+echo "[+] Downloading resolvers.txt for shuffledns."
+wget -q https://raw.githubusercontent.com/janmasarik/resolvers/master/resolvers.txt
+echo "[+] Done."
+
+echo "[+] Downloading ffuf."
+go get -u github.com/ffuf/ffuf
+echo "[+] Done."
+
+echo "[+] Installing subjack."
+go get github.com/haccer/subjack
+echo "[+] Done."
+
+echo "[+] Installing nuclei."
+GO111MODULE=on go get -u -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+git clone https://github.com/projectdiscovery/nuclei-templates.git
+echo "[+] Done."
+
+echo "[+] Installing massdns."
+git clone https://github.com/blechschmidt/massdns.git 
+make -C massdns/
+cp massdns/bin/massdns /usr/bin/
+echo "[+] Done."
+
+echo "[+] Installing nmap."
+sudo apt install nmap -y
+echo "[+] Done."
+
+echo "[+] Installing dnscan."
+git  clone https://github.com/rbsec/dnscan.git  > /dev/null 2>&1
+pip3 -q install -r dnscan/requirements.txt > /dev/null 2>&1
+echo "[+] Done"
+
+echo "[+] Installing httpx"
+GO111MODULE=on go get -u github.com/projectdiscovery/httpx/cmd/httpx > /dev/null 2>&1
+echo "[+] Done."
+
+echo "[+] Downloading wordlist for dns-bruteforcing."
+wget -q https://s3.amazonaws.com/assetnote-wordlists/data/manual/best-dns-wordlist.txt -P ./bugbounty/tools/
+echo "[+] Done."
+
+echo "[+] Installing webanalyze"
+go get -v -u github.com/rverton/webanalyze/cmd/webanalyze
+webanalyze -update
+
+echo "[+] Installing unfurl."
+go get -u github.com/tomnomnom/unfurl
+
+echo "[+] Installing github-subdomain search."
+git clone https://github.com/gwen001/github-search.git
+pip3 install -r requirements2.txt
+pip3 install -r requirements2.txt
+
+echo "[+] Installing crobat for subdomain enumeration."
+go get -u github.com/cgboal/sonarsearch/crobat
+
+
+echo "[+]Adding api keys in subfinder config file."
+
+line=$(cat -n ~/.config/subfinder/config.yaml|grep binaryedge: |awk '{print $1}')
+echo $line
+sed -i "${line},\$d" ~/.config/subfinder/config.yaml 
+echo """
+binaryedge:
+  - b63cc93b-5b72-426d-8141-7a22d4927305
+censys:
+  - 29c6e13b-bdd2-46d8-b6b7-1f679b4ee70d:CfEDWOxM2OLR8CYzfNnK0V46E2oLcokd
+certspotter:
+  - 32919_sMCauuiKvIWuWRmdGZll
+chaos:
+  - a01b4a1eae3ec7ea5293318487e2aa83208c428103740e553ddde3bac8e412d1
+dnsdb:
+  - 2f4a5083f44ce5702a7c86f770ed157f3eb98d519ea46ab4c5bed7239e8c660d
+github:
+  - deb5b594aa1ff575f8117f42dd5d3e8b16379473
+intelx:
+  - 57cd7cac-0dfe-4254-ab3e-524a690c3867
+passivetotal:
+  - 6b12dc7b8ddb20b42f59a0398d5fe31ba4beb247464602f9ce50d4de71ff8bc7
+recon: []
+robtex:
+  - 51BHtnjT9TfRONnE1ud8
+securitytrails:
+  - UAwTto0HEx60SUpX6lS6Qd8gfvLKjwbk
+shodan:
+  - jQIqdSSf8czWjZcF1EHzfPtyLQGfxpOE
+spyse:
+  - b60e4d57-f03a-4820-a8c6-49e94a257e13
+threatbook: []
+urlscan:
+  - 7c858bcf-d91b-478e-bb3e-518420fcbf19
+virustotal:
+  - 535864afe283421ea46b62ad6c2174a0d1cf7dd850a2ce38d02cb9136f24895e
+zoomeye:
+  - 26382A3A-6162-c2693-6631-dc25d62ff6f
+subfinder-version: 2.4.5
+""" >> ~/.config/subfinder/config.yaml
